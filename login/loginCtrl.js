82e9160dea7444a3931ed1bdbbcc5f2e17de9409
Bryan Smith <b.smith33@hotmail.com>
2015-05-18 17:06:03 -0600

    more login page work

--- login/loginCtrl.js
+++ login/loginCtrl.js
@@ -1,9 +1,17 @@
 var app = angular.module('blog-hopper');
 
-app.controller('loginCtrl', function($scope){
 
-	$scope.isCollapsed = false;
+app.controller('loginCtrl', function($scope) {
 
-
+    $scope.userInput = true;
+    $scope.toggle1 = function() {
+        $scope.userInput = !$scope.userInput;
+    }
+    $scope.bloggerInput = true;
+    $scope.toggle2 = function() {
+        $scope.bloggerInput = !$scope.bloggerInput;
+    }
 
 });//end
+
+ 
